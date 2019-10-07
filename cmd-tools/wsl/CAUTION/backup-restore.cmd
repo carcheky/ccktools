344fa8e2d70d6b0440cbac9e5d44658baf2c21db
carcheky <carcheky@gmail.com>
2019-10-07 22:17:10 +0200

    cmd-tools/wsl/CAUTION/backup-restore:

--- cmd-tools/wsl/CAUTION/backup-restore.cmd
+++ cmd-tools/wsl/CAUTION/backup-restore.cmd
@@ -3,6 +3,7 @@ pushd "%~dp0"
 
 wsl --distribution WslServer echo "Restaurando backup, espera..."
 wsl --import WslServer %SYSTEMDRIVE%\wsl\Distro\WslServer %SYSTEMDRIVE%\wsl\WslDistros\WslServer-backup.tar.gz
+wsl --upgrade WslServer
 
 :exit
 popd
