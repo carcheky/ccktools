ddd418820a8c4a1da476f5cca1fd20f6843609c3
carcheky <carcheky@gmail.com>
2019-10-08 20:12:47 +0200

    backups

--- cmd-tools/wsl/backup.cmd
+++ cmd-tools/wsl/backup.cmd
@@ -2,7 +2,7 @@
 pushd "%~dp0"
 
 wsl --distribution WslServer echo "Haciendo backup, espera..."
-wsl --distribution WslServer mysql-export-all
+wsl --distribution WslServer /home/user/ccktools/bin/mysql-export-all
 wsl --distribution WslServer rm -fr /tmp/*
 wsl --terminate WslServer
 wsl --export WslServer %SYSTEMDRIVE%\wsl\WslDistros\WslServer-backup.tar.gz
