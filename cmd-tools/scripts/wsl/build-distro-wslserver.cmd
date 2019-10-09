38fe860d8c727d62ff3bab9d320c3dd34e9b20af
carcheky <cmartinezv@consultoria.ilunion.com>
2019-10-09 16:25:41 +0200

    cmd-tools/scripts/wsl/build-distro-wslserver:

--- cmd-tools/scripts/wsl/build-distro-wslserver.cmd
+++ cmd-tools/scripts/wsl/build-distro-wslserver.cmd
@@ -2,13 +2,17 @@
 pushd "%~dp0"
 
 @echo on
-echo "Haciendo backup, espera..."
+echo "Exportando BBDD, espera..."
 @echo off
 wsl --terminate WslServer
 wsl --distribution WslServer --user user /home/user/ccktools/bin/lamp start
 wsl --distribution WslServer --user user /home/user/ccktools/bin/build-distro-wslserver export
 wsl --terminate WslServer
+
+echo "Haciendo backup, espera..."
 wsl --export WslServer %SYSTEMDRIVE%\wsl\Wsl_Backups\BUILD-WslServer-backup-DISTRO.tar.gz
+
+echo "Importando BBDD, espera..."
 wsl --distribution WslServer --user user /home/user/ccktools/bin/lamp start
 wsl --distribution WslServer --user user /home/user/ccktools/bin/build-distro-wslserver import
 wsl --terminate WslServer
