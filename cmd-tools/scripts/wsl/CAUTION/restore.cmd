5a307b3d0790c57f06a70213a3f827832f238e41
carcheky <cmartinezv@consultoria.ilunion.com>
2019-10-09 13:38:56 +0200

    cmd-tools/scripts/wsl/caution/restore:

--- cmd-tools/scripts/wsl/CAUTION/restore.cmd
+++ cmd-tools/scripts/wsl/CAUTION/restore.cmd
@@ -3,12 +3,10 @@ pushd "%~dp0"
 
 echo Importando Backup WslServer, espera...
 wsl --import WslServer %APPDATA%\WslServer %SYSTEMDRIVE%\wsl\Wsl_Backups\WslServer-backup-DISTRO.tar.gz
-REM wsl --upgrade WslServer
-
 
 wsl -s WslServer
-powershell.exe -File C:\wsl\user\ccktools\cmd-tools\scripts\wsl\set-default-user.ps1
 
+pause
 :exit
 popd
 @echo on
