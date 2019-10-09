8541b55b82ed0bf7f01c885075a701fba826408f
carcheky <cmartinezv@consultoria.ilunion.com>
2019-10-09 13:43:04 +0200

    cmd-tools/scripts/wsl/caution:

--- cmd-tools/scripts/wsl/CAUTION/restore.cmd
+++ cmd-tools/scripts/wsl/CAUTION/restore.cmd
@@ -1,6 +1,13 @@
 @echo off
 pushd "%~dp0"
 
+
+wsl --distribution WslServer echo "Desistalando WslServer, espera..."
+set TARGET_DIR=%APPDATA%\WslServer
+wsl --unregister WslServer
+if exist %TARGET_DIR% rmdir /s /q %TARGET_DIR%
+
+
 echo Importando Backup WslServer, espera...
 wsl --import WslServer %APPDATA%\WslServer %SYSTEMDRIVE%\wsl\Wsl_Backups\WslServer-backup-DISTRO.tar.gz
 
