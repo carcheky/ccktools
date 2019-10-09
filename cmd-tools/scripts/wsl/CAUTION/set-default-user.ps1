e0ead7fb3bbbfaedf5bce1aa0b518f857a662f0c
carcheky <cmartinezv@consultoria.ilunion.com>
2019-10-09 15:50:13 +0200

    multiple fixes

--- cmd-tools/scripts/wsl/CAUTION/set-default-user.ps1
+++ cmd-tools/scripts/wsl/CAUTION/set-default-user.ps1
@@ -1,4 +1,8 @@
+
 echo seleccionando usuario...
-Function WSL-SetDefaultUser ($distro, $user) { Get-ItemProperty Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Lxss\*\ DistributionName | Where-Object -Property DistributionName -eq $distro | Set-ItemProperty -Name DefaultUid -Value ((wsl -d $distro -u $user -e id -u) | Out-String); };
-WSL-SetDefaultUser WslServer user
-sleep 3
+
+sleep 13
+
+  Get-ItemProperty Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Lxss\*\ DistributionName | Where-Object -Property DistributionName -eq WslServer | Set-ItemProperty -Name DefaultUid -Value ((wsl -d WslServer -u user -e id -u) | Out-String);
+
+sleep 13
--- cmd-tools/scripts/wsl/CAUTION/uninstall.cmd
+++ cmd-tools/scripts/wsl/CAUTION/uninstall.cmd
@@ -1,4 +1,4 @@
-@echo on
+@echo off
 pushd "%~dp0"
 
 @echo on
