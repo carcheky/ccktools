07bc29e4e6640025db56b13578ac4f8f8b1446aa
carcheky <cmartinezv@consultoria.ilunion.com>
2019-10-11 10:22:18 +0200

    wsl-manager:

--- cmd-tools/wsl-manager.cmd
+++ cmd-tools/wsl-manager.cmd
@@ -1,3 +1,1 @@
-
-REM start /MIN  "WslManager" "C:\wsl\user\ccktools\cmd-tools\scripts\gui.cmd"
 wscript.exe "C:\wsl\user\ccktools\cmd-tools\scripts\invisible.vbs" "C:\wsl\user\ccktools\cmd-tools\scripts\gui.cmd"
