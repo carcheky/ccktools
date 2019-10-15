ab107c93c71ecfec1349628b3fc3c6cfef9a247e
carcheky <carcheky@gmail.com>
2019-10-15 12:50:07 +0200

    scripts/gui:

--- cmd-tools/scripts/gui.cmd
+++ cmd-tools/scripts/gui.cmd
@@ -35,6 +35,9 @@ GOTO :1
 :9
   start C:\wsl\user\ccktools\cmd-tools\scripts\wsl\update-ccktools.cmd
   GOTO :GUI
+:10
+  start C:\wsl\user\ccktools\cmd-tools\scripts\wsl\build-distro-wslserver.cmd
+  GOTO :GUI
 GOTO :EOF
 -->
 <HTML>
@@ -45,7 +48,7 @@ ICON="..\IMG\icon.ico"
 SYSMENU="no"
 SCROLL="yes"
 BORDER="none"
-BORDERSTYLE="sunken"
+BORDERSTYLE="normal"
 REM APPLICATIONNAME="WslServer Manager"
 REM CAPTION="yes"
 REM MAXIMIZEBUTTON="yes"
@@ -57,9 +60,9 @@ REM WINDOWSTATE="maximize"
 />
 <TITLE>WslServer Manager</TITLE>
 <SCRIPT language="JavaScript">
-window.resizeTo(200,220);
+window.resizeTo(200,120);
 WindowLeft = (window.screen.availWidth - 200)  ;
-WindowTop  = (window.screen.availHeight - 220);
+WindowTop  = (window.screen.availHeight - 120);
 window.moveTo( WindowLeft, WindowTop);
 function closeHTA(reply){
      var fso = new ActiveXObject("Scripting.FileSystemObject");
@@ -69,61 +72,84 @@ function closeHTA(reply){
 </SCRIPT>
 <style>
 body{
-    font-family:  Arial;
+  font-family:  comic-sans;
 }
 button{
-    border: 2px solid black;
+  border: 1px solid black;
   background: gray;
   color: white;
-  font-weight: bold;
-  font-size: 12px;
   width: 98%;
   margin: 1%;
   cursor: pointer;
   display: block;
 }
 button:hover{
-
 }
 .lamp_buttons button{
-
+  font-weight: bold;
 }
 .lamp_buttons .start{
-    background: green;
+  background: green;
 }
 .lamp_buttons .stop{
-    background: red;
+  background: red;
 }
 .lamp_buttons .restart{
-    background: orange;
+  background: orange;
+}
+.exit_buttons{
+  width: 80%;
 }
 .exit_buttons button{
-    float: right;
+  float: left;
   background: black;
-  font-size: 8px;
+  font-size: 12px;
+  margin: 1%;
+  padding: 0;
   width: auto;
 }
-.exit_buttons .exit{
+.exit_buttons button.exit{
+  font-size: 15px;
   border-radius: 100px;
-background: red;
+  background: red;
+  margin: 0;
+  padding:2px 5px;
+  float:right;
+  border: 1px solid black;
+  font-weight: 400 !important;
+  position:absolute;
+  top: 2px;
+  right: 0;
+}
+hr{
+  clear: both;
+  width: 100%;
 }
 </style>
+<meta http-equiv="x-ua-compatible" content="ie=edge" />
 </HEAD>
 <BODY>
   <div class="exit_buttons">
     <button class="exit" onclick="closeHTA(1);" title="cerrar">X</button>
     <button class="terminal" onclick="closeHTA(8);" title="abre una nueva terminal">terminal</button>
     <button class="kill" onclick="closeHTA(6);" title="mata todos los procesos del servidor">kill</button>
-    <button class="update" onclick="closeHTA(9);" title="actualiza el programita">update</button>
+    <button class="newvhost" onclick="closeHTA(7);" title="Asistente para crear un nuevo virtual host con drupal instalado">+</button>
   </div>
   <div class="lamp_buttons flex">
     <button class="start" onclick="closeHTA(2);" title="Arranca el servidor apache + mysql">START</button>
-    <button class="stop" onclick="closeHTA(3);" title="Para el servidor apache + mysql">STOP</button>
     <button class="restart" onclick="closeHTA(4);" title="Reinicia el servidor apache + mysql">RESTART</button>
+    <button class="stop" onclick="closeHTA(3);" title="Para el servidor apache + mysql">STOP</button>
     <hr>
-    <button class="newvhost" onclick="closeHTA(7);" title="Asistente para crear un nuevo virtual host con drupal instalado">NEW VHOST</button>
+    <button class="newvhost" onclick="closeHTA(7);" title="Asistente para crear un nuevo virtual host con drupal instalado">+ CREAR SITIO</button>
     <hr>
     <button class="backup" onclick="closeHTA(5);" title="copia de seguridad de tu servidor">BACKUP</button>
+    <button class="backup" onclick="closeHTA(10);" title="Exporta la distribución linux sin bases de datos para importar en otro pc">BUILD</button>
+  </div>
+    <hr>
+  <div class="exit_buttons">
+    <button class="terminal" onclick="closeHTA(8);" title="abre una nueva terminal">terminal</button>
+    <button class="kill" onclick="closeHTA(6);" title="mata todos los procesos del servidor">kill</button>
+    <button class="update" onclick="closeHTA(9);" title="actualiza el programita">update</button>
   </div>
 </BODY>
 </HTML>
