b52d1f736a1ffcb21236a612b72eb2d49817882d
carcheky <carcheky@gmail.com>
2019-10-15 14:46:57 +0200

    scripts/lamp/lamp-vhostnew:

--- cmd-tools/scripts/lamp/lamp-vhostnew.cmd
+++ cmd-tools/scripts/lamp/lamp-vhostnew.cmd
@@ -4,7 +4,8 @@ pushd "%~dp0"
 echo "newvhost"
 @echo off
 wsl --distribution WslServer --user user bash /home/user/ccktools/bin/vhostnew
-
+wsl --distribution WslServer --user user /home/user/ccktools/bin/wsl-notify VHOST CREATED
+pause
 :exit
 exit
 ::popd
