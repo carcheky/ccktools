d6bba6a63eec2024bddd9dc0aa5195b653c719fe
carcheky <carcheky@gmail.com>
2020-01-09 22:57:19 +0100

    fix-bin

---
+++ cmd-tools/scripts/wsl/fix-bin.cmd
@@ -0,0 +1,13 @@
+/mnt/c/wsl/user/ccktools/bin
+
+
+@echo off
+pushd "%~dp0"
+
+wsl --terminate WslServer
+wsl --distribution WslServer --user user /home/user/ccktools/bin/fix-bin
+
+:exit
+exit
+::popd
+@echo on
