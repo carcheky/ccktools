b4eca2c8181674c422e3e0d3a04d1f137e91d392
carcheky <carcheky@gmail.com>
2020-02-11 19:33:14 +0100

    cmd-tools/scripts/wsl/build-distro-wslserver:  added date

--- cmd-tools/scripts/wsl/build-distro-wslserver.cmd
+++ cmd-tools/scripts/wsl/build-distro-wslserver.cmd
@@ -4,15 +4,20 @@ pushd "%~dp0"
 @echo on
 echo "Exportando BBDD, espera..."
 @echo off
+
+for /f %%x in ('wmic path win32_utctime get /format:list ^| findstr "="') do set %%x
+set today=%Year%-%Month%-%Day%-%Hour%-%Minute%-%Second%
+
 wsl --terminate WslServer
 wsl --distribution WslServer --user user /home/user/ccktools/bin/lamp start
+wsl --distribution WslServer --user user /home/user/ccktools/bin/mysql-export-all %today%
 wsl --distribution WslServer --user user /home/user/ccktools/bin/build-distro-wslserver export
 wsl --terminate WslServer
 
-echo "Haciendo backup, espera..."
-wsl --export WslServer %SYSTEMDRIVE%\wsl\Wsl_Backups\BUILD-WslServer-backup-DISTRO.tar.gz
+echo "Compilando, espera..."
+wsl --export WslServer %SYSTEMDRIVE%\wsl\Wsl_Backups\BUILD-WslServer-backup-DISTRO-%today%.tar.gz
 
-echo "Importando BBDD, espera..."
+echo "Restaurando BBDD, espera..."
 wsl --distribution WslServer --user user /home/user/ccktools/bin/lamp start
 wsl --distribution WslServer --user user /home/user/ccktools/bin/build-distro-wslserver import
 wsl --terminate WslServer
