8ba732122fe8361e15c0de666b3085d80030e248
carcheky <carcheky@gmail.com>
2020-02-11 19:33:00 +0100

    cmd-tools/scripts/wsl/backup: added date

--- cmd-tools/scripts/wsl/backup.cmd
+++ cmd-tools/scripts/wsl/backup.cmd
@@ -4,11 +4,15 @@ pushd "%~dp0"
 @echo on
 echo "Haciendo backup, espera..."
 @echo off
+
+for /f %%x in ('wmic path win32_utctime get /format:list ^| findstr "="') do set %%x
+set today=%Year%-%Month%-%Day%-%Hour%-%Minute%-%Second%
+
 wsl --terminate WslServer
 wsl --distribution WslServer --user user /home/user/ccktools/bin/lamp start
-wsl --distribution WslServer --user user /home/user/ccktools/bin/mysql-export-all
+wsl --distribution WslServer --user user /home/user/ccktools/bin/mysql-export-all %today%
 wsl --terminate WslServer
-wsl --export WslServer %SYSTEMDRIVE%\wsl\Wsl_Backups\WslServer-backup-DISTRO.tar.gz
+wsl --export WslServer %SYSTEMDRIVE%\wsl\Wsl_Backups\WslServer-backup-DISTRO--%today%.tar.gz
 
 :exit
 exit
