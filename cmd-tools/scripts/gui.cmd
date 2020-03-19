5bcb218236797362a039fa0d4cfa5e65bddc796f
carcheky <carcheky@gmail.com>
2020-03-19 09:39:05 +0100

    cmd-tools/scripts/gui:

--- cmd-tools/scripts/gui.cmd
+++ cmd-tools/scripts/gui.cmd
@@ -13,7 +13,7 @@ GOTO :1
 :: LAMP TOOLS
 :2
   start C:\wsl\user\ccktools\cmd-tools\scripts\lamp\lamp-start.cmd
-  start C:\wsl\user\ccktools\cmd-tools\scripts\windows\TDFR.exe
+  start C:\wsl\Q\ToggleDefender.exe
   GOTO :1
 :3
   start C:\wsl\user\ccktools\cmd-tools\scripts\lamp\lamp-stop.cmd
@@ -50,7 +50,7 @@ GOTO :1
   wscript.exe "C:\wsl\user\ccktools\cmd-tools\scripts\invisible.vbs" "C:\wsl\user\ccktools\cmd-tools\scripts\gui-backupgui.cmd"
   GOTO :GUI
 :14
-  start C:\wsl\user\ccktools\cmd-tools\scripts\windows\TDFR.exe
+  start C:\wsl\Q\ToggleDefender.exe
   GOTO :GUI
 GOTO :1
 
